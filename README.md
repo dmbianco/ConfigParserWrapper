ae8fdb76da09f8ca5240a4ebf65c349977aa66a5
Domenico M. Bianco <domenicom.bianco@gmail.com>
2017-10-04 23:24:25 +0200

    code commented

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # ConfigParserWrapper
-#### A wrapper for Python class ConfigParser
+### A wrapper for Python class ConfigParser
+
+
+The intent of this class is to provide additional functionalities to the ConfigParser
+class. In particular, it can infer the type of the pickled variables.
+Only a bunch of types are supported (str, int, float, dict, bool), but they can easily
+extended (give a look at the dictionary *types* into the class definition).
+
+## Usage
+
+An instance of ConfigParserWrapper can be initialized with the constructor of the class. It only requires a dict that should have a structure similar to the following:
+```
+dict = { "section1":{"var1":"val1", "var2":"val2"},
+         "section2":{"varA":"valA", "varB":"valB"}
+        }
+```
+Alternatively, a *section* (using the jargon of ConfigParser) can be added thanks to the method 
+```
+add_section_dict(dic_of_variables, section_name, check_section=True)
+```
+
+The easiest way to use this class is through the method *add_section_dict*. Then one calls the method *save_config* to save all the variables. Finally, one can restore the variables and the associated values (hopefully with the correct types) with *load_config*.
+
+
+
 
 
 
--- config_parser_wrapper.py
+++ config_parser_wrapper.py
@@ -6,6 +6,7 @@ import ast
 # types of the values correctly.
 
 def boolean_parser(string):
+    # It parses a string looking for a boolean interpretable value
     string = str.lower(string)
     if string in ['true', '1', 't', 'y', 'yes']:
         return True
@@ -16,19 +17,26 @@ def boolean_parser(string):
 
 
 class ConfigParserWrapper:
-    #
+    # The recognized/supported types are defined into the _types dictionrary.
+    # The regex _extract_type extracts the type of a varible from Python output,
+    # e.g. "<type 'int'>".
+    # The regex _get_type extracts the type of a restored variable since it is saved
+    # following the pattern "type_name".
 
-    types = {"int":int, "float":float, "dict":ast.literal_eval, "str":str,
+    _types = {"int":int, "float":float, "dict":ast.literal_eval, "str":str,
             "bool":boolean_parser}
-    extract_type = re.compile("'(.+)'>$")
-    get_type = re.compile("^([^_]+)")
+    _extract_type = re.compile("'(.+)'>$")
+    _get_type = re.compile("^([^_]+)")
     
 
-    def __init__(self, dic=None):
+    def __init__(self, dic={}):
+        # Constructor. The keys of dic should be sections and its values should be
+        # dictionaries of configuration variables.
         self.dic = dic
     
 
     def add_section_dict(self, dic_of_variables, section_name, check_section=True):
+        # It adds the dictionary dic_of_variables to self.dic with the key section_name.
         if section_name not in self.dic or not check_section:
             self.dic[section_name] = dic_of_variables
         else:
@@ -36,12 +44,13 @@ class ConfigParserWrapper:
     
 
     def save_config(self, output_file="parameters.cfg"):
+        # It saves the configurations.
         config = ConfigParser.RawConfigParser()
         for section,dic_of_variables in self.dic.iteritems():
             config.add_section(section)
             for var,value in dic_of_variables.iteritems():
                 type_of_var = str(type(value))
-                type_of_var = re.findall(self.extract_type, type_of_var)[0]
+                type_of_var = re.findall(self._extract_type, type_of_var)[0]
                 config.set(str(section), str(type_of_var) + "_" + str(var),str(value))
 
         with open(output_file, 'wb') as configfile:
@@ -49,6 +58,10 @@ class ConfigParserWrapper:
 
 
     def load_config(self, input_file="parameters.cfg", flatten=False):
+        # It restores the configuration variables from a file. If flatten is true, 
+        # then the hierarchy section:dict_of_variables is flattened and all variables 
+        # are saved in the same dictionary; notice that homonymous variables could
+        # lead to unintended results in this case.
         config = ConfigParser.ConfigParser()
         config.read(input_file)
 
@@ -65,11 +78,13 @@ class ConfigParserWrapper:
 
 
     def _parse_value(self, variable, value):
-        var_type = re.findall(self.get_type, variable)[0]
-        variable = re.sub(self.get_type, "", variable, 1)[1:]
+        # It parses the name of a saved variable that is in the form "type_name" and
+        # returns only its name and the cast value.
+        var_type = re.findall(self._get_type, variable)[0]
+        variable = re.sub(self._get_type, "", variable, 1)[1:]
         converter = None
-        if var_type in self.types:
-            converter = self.types[var_type]
+        if var_type in self._types:
+            converter = self._types[var_type]
             
         if converter:
             return variable, converter(value)
